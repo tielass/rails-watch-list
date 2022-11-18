df57ff4f7c1c1c617a18705ec6810ad050216d4d
Mihail Mihaylov <tielass@gmail.com>
2022-11-18 12:41:31 +0100

    done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
-  has_many :movies, through: :bookmarks
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true
   validates :name, uniqueness: true
 end
